3ddc37ba0261aa800f3107475cf7ab6d36a0afd8
saleira <goncalo.caro@gmail.com>
2025-03-06 22:31:19 +0000

    Updated cached data event

--- src/api.js
+++ src/api.js
@@ -39,7 +39,7 @@ export const getEvents = async () => {
     if (!navigator.onLine) {
         const events = localStorage.getItem('lastEvents');
         NProgress.done();
-        return events ? JSON.parse(events).events : [];
+        return events ? JSON.parse(events): [];
     }
 
     const token = await getAccessToken();
